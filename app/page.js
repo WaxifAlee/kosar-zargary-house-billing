ee96e72325588f12b6157fa8ba8893d606cf8a7b
WaxifAlee <waxif.alee1@gmail.com>
2024-01-18 10:38:35 +0500

    pages structure added

--- app/page.js
+++ app/page.js
@@ -12,26 +12,31 @@ const Home = () => {
         <h2 className="text-2xl mb-5 font-sans from-stone-200">
           Please Select a Menu
         </h2>
+        <h5 className="text-xs">- KOSAR ZARGARY HOUSE -</h5>
 
         <div
           className="flex justify-center
                       flex-col rounded"
         >
-          <Link className="p-3 m-2 rounded bg-blue-400 w-64" href={"/"}>
-            {" "}
-            Sale Bill{" "}
+          <Link
+            className="p-3 m-2 rounded bg-blue-400 w-64"
+            href={"/pages/sale"}
+          >
+            Sale Bill
           </Link>
 
-          <Link className="p-3 m-2 rounded bg-green-400 w-64" href={"/"}>
-            {" "}
-            Buy Bill{" "}
+          <Link
+            className="p-3 m-2 rounded bg-green-500 w-64"
+            href={"/pages/buy"}
+          >
+            Buy Bill
           </Link>
 
-          <Link className="p-3 m-2 rounded bg-yellow-400 w-64" href={"/"}>
-            {" "}
-            Gold Rate{" "}
+          <Link className="p-3 m-2 rounded bg-yellow-600 w-64" href={"/"}>
+            Gold Rate
           </Link>
         </div>
+        <p className="text-xs text-gray-100">Developed By: Wasif Ali</p>
       </div>
     </div>
   );
---
+++ app/pages/sale/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Buy = () => {
+  return (
+    <div className="text-center">
+      <h2 className="p-2 mt-2 border-2 text-lg">Sale Billing</h2>
+    </div>
+  );
+};
+
+export default Buy;
